1139025b7914455044cc7abf340025654614f701
Nagacash <chosenfewrecords@hotmail.de>
2022-11-14 23:38:17 +0000

    first

--- pages/index.js
+++ pages/index.js
@@ -1,64 +1,69 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import CoinGecko from 'coingecko-api';
+import { gray } from 'colors';
 const coinGeckoClient = new CoinGecko();
 
 export default function Home(props) {
   const { data } = props.result;
-  
-  const formatPercent = number => 
-      `${new Number(number).toFixed(2)}%`
+
+  const formatPercent = number =>
+    `${new Number(number).toFixed(2)}%`
 
   const formatDollar = (number, maxSignificantDigits) =>
-      new Intl.NumberFormat(
-        'en-US',
-        {
-          style: 'currency',
-          currency: 'usd',
-          maxSignificantDigits
-        })
-        .format(number);
+    new Intl.NumberFormat(
+      'en-US',
+      {
+        style: 'currency',
+        currency: 'usd',
+        maxSignificantDigits
+      })
+      .format(number);
 
   return (
     <div className={styles.container}>
       <Head>
-        <title> CoinMarketCap Top 100 Cryptocurrencies by Market Capitalization </title>
-        <link rel="icon" href="/favicon.ico" />
-        <h1> Coinmarketcap clone | Top 100 Cryptocurrencies by Market Capitalization </h1>
+        <div className={styles.title2}>
+          <title> CoinMarketCap Top 100 Cryptocurrencies by Naga Apparel </title>
+          <link rel="icon" href="/favicon.ico" />
+          <h1> | Top 100 Cryptocurrencies by Naga Apparel </h1>
+        </div>
         <table className='table'>
-         <thead>
-          <tr>
-             <th>Name</th>
-            <th>Symbol</th>
-            <th>24h Change</th>
-            <th>Price</th>
-            <th>Market cap</th>
-          </tr>
-         </thead>
-         <tbody>
-           {data.map(coin => (
-             <tr key={coin.id}>
-               <td>{coin.id}</td>
-               <td>
-                  <img 
-                  src={coin.image}
-                  style={{width: 25, height: 25, marginRight: 10}}
+          <thead>
+            <div className={styles.mix}>
+              <tr>
+                <th>Name</th>
+                <th>Symbol</th>
+                <th>24h Change</th>
+                <th>Price</th>
+                <th>Market cap</th>
+              </tr>
+            </div>
+          </thead>
+          <tbody>
+            {data.map(coin => (
+              <tr key={coin.id}>
+                <td>{coin.id}</td>
+                <td>
+                  <img
+                    src={coin.image}
+                    style={{ width: 25, height: 25, marginRight: 10, }}
                   />
                   {coin.symbol.toUpperCase()}
-               </td>
-               <td>
+                </td>
+                <td>
                   <span
                     className={coin.price_change_percentage_24 > 0 ? (
                       'text-success') : 'text-danger'}
                   >
-               {formatPercent(coin.price_change_percentage_24h)}
+                    {formatPercent(coin.price_change_percentage_24h)}
                   </span>
-               </td>
-               <td>{formatDollar(coin.current_price, 20)}</td>
-               <td>{formatDollar(coin.market_cap, 12)}</td>
-             </tr>
-           ))}
-         </tbody>
+                </td>
+                <td>{formatDollar(coin.current_price, 20)}</td>
+                <td>{formatDollar(coin.market_cap, 12)}</td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </Head>
     </div>
@@ -69,7 +74,7 @@ export async function getServerSideProps(context) {
   const params = {
     order: CoinGecko.ORDER.MARKET_CAP_DESC
   }
-  const result = await coinGeckoClient.coins.markets({params});
+  const result = await coinGeckoClient.coins.markets({ params });
   return {
     props: {
       result
